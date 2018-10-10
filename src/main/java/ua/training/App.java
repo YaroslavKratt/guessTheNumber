a96b9dd3c2e9fb976d3ea67fc20e0746e8787531
Yaroslav <wtf0100@gmail.com>
2018-10-10 19:52:49 +0300

    Adding project

---
+++ src/main/java/ua/training/App.java
@@ -0,0 +1,13 @@
+package ua.training;
+
+/**
+ * This is a guess number game, have fun)
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
